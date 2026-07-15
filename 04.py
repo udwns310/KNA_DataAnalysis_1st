085d434edb8c6e470dc306ce4a4d7ae3ddacc6ca
Choi YoungJun <uroasecret5@gmail.com>
2026-07-15 17:50:06 +0900

    study : 사칙연산 계산기, 평균, 도형

--- 04.py
+++ 04.py
@@ -1,29 +1,57 @@
-count = 7  # 정수형 int
-temp = 3.5  # 실수형 float
-name = "최영준"  # 문자열형 str
-is_ok = True  # 논리형 bool
-# print(count, temp, name, is_ok)
+# count = 7  # 정수형 int
+# temp = 3.5  # 실수형 float
+# name = "최영준"  # 문자열형 str
+# is_ok = True  # 논리형 bool
+# # print(count, temp, name, is_ok)
 
-# print(
-#     type(count), type(temp), type(name), type(is_ok)
-# )  # class<'int'>, <class 'float'>, <class 'str'>, <class 'bool'>>
+# # print(
+# #     type(count), type(temp), type(name), type(is_ok)
+# # )  # class<'int'>, <class 'float'>, <class 'str'>, <class 'bool'>>
 
-# print(
-#     type(100), type(100.0), type("100")
-# )  # <class 'int'>, <class 'float'>, <class 'str'>
+# # print(
+# #     type(100), type(100.0), type("100")
+# # )  # <class 'int'>, <class 'float'>, <class 'str'>
 
-print(3 + 4, "3" + "4", "20" + "30")  # 7, 34, 2030
+# print(3 + 4, "3" + "4", "20" + "30")  # 7, 34, 2030
 
-print(5 > 2, 5 == 5, type(5 > 2))  # True, True, <class 'bool'>
+# print(5 > 2, 5 == 5, type(5 > 2))  # True, True, <class 'bool'>
 
-tmp = 3
-print(tmp, type(tmp)) # 3 <class 'int'> 정수형
-tmp = 3.0 
-print(tmp, type(tmp))   # 3.0 <class 'float'> 실수형
-tmp = "3"
-print(tmp, type(tmp))  # 3 <class 'str'> 문자열형
+# tmp = 3
+# print(tmp, type(tmp)) # 3 <class 'int'> 정수형
+# tmp = 3.0
+# print(tmp, type(tmp))   # 3.0 <class 'float'> 실수형
+# tmp = "3"
+# print(tmp, type(tmp))  # 3 <class 'str'> 문자열형
 
-device_temp = 36.5 # float  
-check_count = 16 # int
-device_name = "모터 B" # str
-is_normal = True # bool
+# device_temp = 36.5 # float
+# check_count = 16 # int
+# device_name = "모터 B" # str
+# is_normal = True # bool
+
+
+# 산술연산자
+# # + - * / //(몫) % (나머지) ** (거듭제곱)
+# print(3 + 5)
+# print(10 - 4)
+# print(6 * 7)
+# print(20 / 4)
+# print(20 // 4)
+# print(20 % 4)
+# print(2 ** 3)
+
+a = 17
+b = 5
+print(a + b, a - b, a * b, a / b, a // b, a % b, a**b)  # 22 12 85 3.4 3 2 1419857
+
+s1 = 80
+s2 = 75
+s3 = 90
+print((s1 + s2 + s3) / 3)  # 81.66666666666667
+
+temp = 8
+print(temp**2)  # 64
+
+x = 4
+y = 5
+z = 6
+print(x * y * z)  # 120
