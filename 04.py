edc0c4c9ed3912f78fda8c55b96fb53986907916
Choi YoungJun <uroasecret5@gmail.com>
2026-07-15 17:01:08 +0900

    타입 실습

---
+++ 04.py
@@ -0,0 +1,29 @@
+count = 7  # 정수형 int
+temp = 3.5  # 실수형 float
+name = "최영준"  # 문자열형 str
+is_ok = True  # 논리형 bool
+# print(count, temp, name, is_ok)
+
+# print(
+#     type(count), type(temp), type(name), type(is_ok)
+# )  # class<'int'>, <class 'float'>, <class 'str'>, <class 'bool'>>
+
+# print(
+#     type(100), type(100.0), type("100")
+# )  # <class 'int'>, <class 'float'>, <class 'str'>
+
+print(3 + 4, "3" + "4", "20" + "30")  # 7, 34, 2030
+
+print(5 > 2, 5 == 5, type(5 > 2))  # True, True, <class 'bool'>
+
+tmp = 3
+print(tmp, type(tmp)) # 3 <class 'int'> 정수형
+tmp = 3.0 
+print(tmp, type(tmp))   # 3.0 <class 'float'> 실수형
+tmp = "3"
+print(tmp, type(tmp))  # 3 <class 'str'> 문자열형
+
+device_temp = 36.5 # float  
+check_count = 16 # int
+device_name = "모터 B" # str
+is_normal = True # bool
